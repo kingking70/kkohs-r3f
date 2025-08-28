daeb081837cf1ee377e637d33fdbdd3af868a804
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-08-28 19:40:58 +0800

    updated anti-network projects to ideas

--- src/Customhtml.jsx
+++ src/Customhtml.jsx
@@ -16,7 +16,7 @@ export default function Customhtml()
             
             <p><a href="https://anti-network.com" target="_blank">anti-network</a>, <a href="https://cli-mate.help" target="_blank">cli-mate.help</a> and newclear.website (coming soon)</p>
             
-            <p>other pending ideas are in anti-network <a href="https://anti-network.com/projects" target="_blank">projects</a></p>
+            <p>other pending ideas are in anti-network's <a href="https://anti-network.com/ideas" target="_blank">ideas</a></p>
             
             <br/>
 

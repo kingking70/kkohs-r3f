171554c63475f033f161bbda8e5b96541648a868
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-08-29 18:09:18 +0800

    updated link for newclear

--- src/Customhtml.jsx
+++ src/Customhtml.jsx
@@ -14,7 +14,7 @@ export default function Customhtml()
 
             <h2>created</h2>
             
-            <p><a href="https://anti-network.com" target="_blank">anti-network</a>, <a href="https://cli-mate.help" target="_blank">cli-mate.help</a> and newclear.website (coming soon)</p>
+            <p><a href="https://anti-network.com" target="_blank">anti-network</a>, <a href="https://cli-mate.help" target="_blank">cli-mate.help</a> and building <a href="https://newclear.website">newclear.website</a> first</p>
             
             <p>other pending ideas are in anti-network's <a href="https://anti-network.com/ideas" target="_blank">ideas</a></p>
             

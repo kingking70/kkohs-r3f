89efa65b2a777153693a341a17fa8b569b2e5cdd
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-10-02 15:07:49 +0800

    rearranged words

--- src/Customhtml.jsx
+++ src/Customhtml.jsx
@@ -7,23 +7,23 @@ export default function Customhtml()
     return <>
         <Html fullscreen transform scale={0.4}>
             <h1>kingston koh</h1>
-            
-            <i>this site is 3d, u should be able to rotate around on web & mobile haha</i> 
+            <i>this site is 3d, u should be able to rotate & zoom around on web & mobile</i> 
             
             <br/>
             <br/> 
-
-            <p>learning r3f to build <a href="https://newclear.website">newclear.website</a></p>
-            <h2>created</h2>
-            
-            <p><a href="https://anti-network.com" target="_blank">anti-network</a> and <a href="https://cli-mate.help" target="_blank">cli-mate.help</a></p>
-            
+            <br/>
+        
+            <p>building <a href="https://cli-mate.help" target="_blank">cli-mate.help</a> and  <a href="https://newclear.website">newclear.website</a></p>
             <p>other pending <a href="https://anti-network.com/ideas" target="_blank">ideas</a> are in anti-network</p>
+            
+            <br/>
+            
+            <h2>created</h2>
+            <p><a href="https://anti-network.com" target="_blank">anti-network</a></p>
             
             <br/>
 
             <h2>wrote</h2>
-
             <p><a href="https://kingstonkoh.com" target="_blank">writings</a></p>
           
         </Html>

2d0c86d8f74912d4a950d3048589decb11da8768
Small_Butterscotch18 <kingston.kdk@gmail.com>
2025-11-16 11:26:45 +0800

    updated on 16 nov 2025

--- src/Customhtml.jsx
+++ src/Customhtml.jsx
@@ -7,25 +7,22 @@ export default function Customhtml()
     return <>
         <Html fullscreen transform scale={0.4}>
             <h1>kingston koh</h1>
-            <i>this site is 3d, u should be able to rotate & zoom around on web & mobile</i> 
+            <i>this site is 3d</i> 
+            <br/>
+            <i>click or pinch the bottom corners on your screen to rotate & zoom around</i> 
             
             <br/>
             <br/> 
             <br/>
-        
-            <p>building <a href="https://cli-mate.help" target="_blank">cli-mate.help</a> and  <a href="https://newclear.website">newclear.website</a></p>
-            <p>other pending <a href="https://anti-network.com/ideas" target="_blank">ideas</a> are in anti-network</p>
+            
+            <h2>creating</h2>
+            <p> <a href="https://anti-network.com" target="_blank">anti-network</a>, <a href="https://cli-mate.help" target="_blank">cli-mate.help</a>, <a href="https://newclear.website">nuclear electricity</a>, <a href="https://balance-medic.com" target="_blank">balance medic</a> and move-well tracker mobile app</p>
             
             <br/>
-            
-            <h2>created</h2>
-            <p><a href="https://anti-network.com" target="_blank">anti-network</a></p>
-            
             <br/>
 
-            <h2>wrote</h2>
-            <p><a href="https://kingstonkoh.com" target="_blank">writings</a></p>
-          
+            <h2>writing</h2>
+            <p> <a href="https://kingstonkoh.com" target="_blank">personal</a>, <a href="https://thelongbuild.substack.com" target="_blank">the long build</a>, and <a href="https://groovydose.substack.com" target="_blank">groovy dose</a> </p>        
         </Html>
         
 
